d5d546ebd6ff93f4074f243fb9bf693ec2771da8
agindi <aarongindi@gmail.com>
2020-08-28 13:28:50 -0400

    Finished functionality of SimpleGrid

--- src/Main.scala
+++ src/Main.scala
@@ -1,9 +1,12 @@
 object Main extends App {
 
   println("Welcome to Sudku")
-
-  var sg = new SimpleGrid(initial = "713814915");
+  val initial = "521822723327228429131933434236737839542844946648155262764566368271373174876577679881982583487188389"
+  var sg = new SimpleGrid(initial = initial)
 
   print(sg.toString)
-  sg.prettyPrint
+  sg.prettyPrint()
+
+  print(sg.valid)
+
 }
--- src/SimpleGrid.scala
+++ src/SimpleGrid.scala
@@ -4,9 +4,9 @@ import Array._
  * This class encapsulates the behavior of a simple 9x9 Sudoku grid, namely:
  *  - options for blank grid or pre-initialized one ✓
  *  - slots for digits 1-9 in a 9x9 grid ✓
- *  - functions determining if the current configuration is valid
+ *  - functions determining if the current configuration is valid ✓
  *  - functions determining if a move results in a valid configuration
- *  - a mutator function returning a new board from the current one making a certain move
+ *  - a mutator function returning a new board from the current one making a certain move ✓
  *  - a function printing the grid as a move string ✓
  *  - a function printing the grid human-readable ✓
  *
@@ -32,19 +32,76 @@ class SimpleGrid(initial : String = "", grid : Array[Array[Short]] = ofDim[Short
     }
   }
 
+  def apply(move : String) : SimpleGrid = {
+    var ng = ofDim[Short](9,9)
+    for (i <- 0 to 8) {
+      for (j <- 0 to 8) {
+        ng(i)(j) = grid(i)(j)
+      }
+    }
 
+    val v = Integer.parseInt(move.substring(0, 1)).toShort
+    val r = Integer.parseInt(move.substring(1, 2))
+    val c = Integer.parseInt(move.substring(2, 3))
 
+    ng(r-1)(c-1) = v
 
-  def valid() : Boolean = {
+    new SimpleGrid(grid = ng)
+  }
+
+  def valid : Boolean = {
+    // iterate over possible values
+    for (v <- 1 to 9) {
+
+      // first check rows
+      for (r <- 0 to 8) {
+        var seen = false // at the start of each row, reset it
+        for (c <- 0 to 8) {
+          if (grid(r)(c) == v) {
+            if (seen) return false
+            seen = true
+          }
+        }
+      }
+
+      // check columns
+      for (c <- 0 to 8) {
+        var seen = false // at the start of each column, reset it
+        for (r <- 0 to 8) {
+          if (grid(r)(c) == v) {
+            if (seen) return false
+            seen = true
+          }
+        }
+      }
+
+      // check boxes
+      for (br <- 0 to 2) {
+        for (bc <- 0 to 2) {
+          var seen = false // reset at the start of each box
+          for (r <- br*3 to (br*3 + 2)) {
+            for (c <- bc*3 to (bc*3 + 2)) {
+              if (grid(r)(c) == v) {
+                if (seen) return false
+                seen = true
+              }
+            }
+          }
+        }
+      }
+
+    }
+
     true
   }
 
   def valid_after(move : String) : Boolean = {
-    true
+    val ng = apply(move)
+    ng.valid
   }
 
 
-  def prettyPrint : Unit = {
+  def prettyPrint() : Unit = {
     println()
     for (c <- 1 to 9) {
       for (r <- 1 to 9) {
@@ -59,7 +116,7 @@ class SimpleGrid(initial : String = "", grid : Array[Array[Short]] = ofDim[Short
     for (c <- 1 to 9) {
       for (r <- 1 to 9) {
         if (grid(r-1)(c-1) != 0) {
-          str += (""+grid(r-1)(c-1)) + r + c;
+          str += (""+grid(r-1)(c-1)) + r + c
         }
       }
     }
